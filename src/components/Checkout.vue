10a86efca1b3665f9f950c1c7957efa95da5052a
Bartłomiej Świątek <emini1@swiatek.biz>
2020-03-30 18:34:38 +0200

    Checkout and OrderThanks

---
+++ src/components/Checkout.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col bg-dark text-white">
+                    <div class="navbar-brand">SKLEP SPORTOWY</div>
+                </div>
+            </div>
+        </div>
+        <div class="m-2">
+            <div class="form-group m-2">
+                <label>Imię</label>
+                <input type="text" v-model="name" class="form-control">
+            </div>
+        </div>
+        <div class="text-center">
+            <router-link to="/cart" class="btn btn-secondary m-1">
+                Powrót
+            </router-link>
+            <button v-on:click="submitOrder" class="btn btn-primary m-1">
+                Złóż zamówienie
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            name: null
+        }
+    },
+    methods: {
+        submitOrder() {
+            //todo: zapisz zamówienie
+        }
+    }
+}
+</script>
