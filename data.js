d355c25b51cd46039dab09bc926853ba3938df64
Bartłomiej Świątek <emini1@swiatek.biz>
2020-04-01 19:43:46 +0200

    pagination

--- data.js
+++ data.js
@@ -1,59 +1,24 @@
-var data = [
-    {
-        id: 1,
-        name: "Kajak",
-        category: "Sporty wodne",
-        description: "Kajak dla jednej osoby",
-        price: 275
-    },
-    {
-        id: 2,
-        name: "Kamizelka ratunkowa",
-        category: "Sporty wodne",
-        description: "Skuteczna i modna",
-        price: 48.95
-    },
-    {
-        id: 3,
-        name: "Piłka nożna",
-        category: "Piłka nożna",
-        description: "Odpowiednia waga i wymiar",
-        price: 19.50
-    },
-    {
-        id: 4,
-        name: "Chorągiewki",
-        category: "Piłka nożna",
-        description: "Profesjonalne",
-        price: 35.95
-    },
-    {
-        id: 5,
-        name: "Chwiejne krzesło",
-        category: "Szachy",
-        description: "Irytuje przeciwnika",
-        price: 29.95
-    },
-    {
-        id: 6,
-        name: "Szachownica",
-        category: "Szachy",
-        description: "Zabawa dla całej rodziny",
-        price: 29.95
-    },
-    {
-        id: 7,
-        name: "Król złoty",
-        category: "Szachy",
-        description: "Pozłacana figura króla",
-        price: 1200
-    },
-]
+var faker = require("faker");
+var data = [];
+var categories = ["Sporty wodne", "Piłka nożna", "Szachy", "Bieganie"];
+faker.locale = "pl";
+faker.seed(100);
+
+for (let i=1; i<=500; i++) {
+    var category = faker.helpers.randomize(categories);
+    data.push({
+        id: i,
+        name: faker.commerce.productName(),
+        category: category,
+        description: `${category}: ${faker.lorem.sentence(3)}`,
+        price: faker.commerce.price()
+    })
+}
 
 module.exports = function () {
     return {
         products: data,
-        categories: [...new Set(data.map(p => p.category))].sort(),
+        categories: categories,
         orders: []
     }
 }
--- src/components/PageControls.vue
+++ src/components/PageControls.vue
@@ -1,34 +1,58 @@
 <template>
     <div class="row mt-2">
-        <div class="col form-group">
+        <div class="col-3 form-group">
             <select v-on:change="changePageSize" class="form-control">
                 <option value="4">4 na stronę</option>
                 <option value="8">8 na stronę</option>
                 <option value="12">12 na stronę</option>
             </select>
         </div>
-        <div v-if="pageCount > 1" class="text-right col">
-            <div class="btn-group mx-2">
-                <button v-for="i in pageNumbers" v-bind:key="i" class="btn"
-                v-bind:class="{'btn-primary': i == currentPage, 'btn-secondary': i != currentPage}"
-                v-on:click="setCurrentPage(i)">{{ i }}</button>
-            </div>
+        <div class="text-right col">
+            <button v-bind:disabled="currentPage == 1" v-on:click="setCurrentPage(currentPage - 1)"
+                class="btn btn-secondary mx-1">Poprzednia</button>
+            <span v-if="currentPage > 4">
+                <button v-on:click="setCurrentPage(1)" class="btn btn-secondary mx-1">1</button>
+                <span class="h4">...</span>
+            </span>
+            <span class="mx-1">
+                <button v-for="i in pageNumbers" v-bind:key="i"
+                    v-bind:class="{ 'btn-primary' : i == currentPage }"
+                    v-on:click="setCurrentPage(i)" 
+                    class="btn btn-secondary">{{i}}</button>
+            </span>
+            <span v-if="currentPage <= pageCount - 4">
+                <span class="h4">...</span>
+                <button v-on:click="setCurrentPage(pageCount)"
+                    class="btn btn-secondary mx-1">{{ pageCount }}</button>
+            </span>
+            <button v-bind:disabled="currentPage == pageCount" v-on:click="setCurrentPage(currentPage + 1)"
+                class="btn btn-secondary mx-1">Następna</button>
         </div>
     </div>
 </template>
 
 <script>
-import { mapState, mapGetters, mapMutations } from "vuex";
+import { mapState, mapGetters, mapActions } from "vuex";
 export default {
     computed: {
         ...mapState(["currentPage"]),
         ...mapGetters(["pageCount"]),
         pageNumbers() {
-            return [...Array(this.pageCount + 1).keys()].slice(1);
+            if (this.pageCount < 4) {
+                return [...Array(this.pageCount + 1).keys()].slice(1);
+            }
+            if (this.currentPage <= 4) {
+                return [1, 2, 3, 4, 5];
+            }
+            if (this.currentPage > this.pageCount - 4) {
+                return [...Array(5).keys()].reverse().map(v => this.pageCount - v);
+            }
+
+            return [this.currentPage - 1, this.currentPage, this.currentPage + 1]
         }
     },
     methods: {
-        ...mapMutations(["setCurrentPage", "setPageSize"]),
+        ...mapActions(["setCurrentPage", "setPageSize"]),
         changePageSize($event) {
             this.setPageSize(Number($event.target.value));
         }
